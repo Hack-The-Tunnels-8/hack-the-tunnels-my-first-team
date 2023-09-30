4878bf176b582165ab6f15270b920a10f93af802
danielyang21 <danielyanxiaoyang21@gmail.com>
2023-09-30 15:36:54 -0400

    Adding footer name and colour

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 import "./Footer.style.scss";
 
 function Footer() {
-  return <div className="© 2023 Carleton University">footer</div>;
+  return <div className="footer">© 2023 Carleton University</div>;
 }
 
 export default Footer;
