1901c3b946c10c9414ad406914fdf7b6822be9f3
danielyang21 <danielyanxiaoyang21@gmail.com>
2023-09-30 15:55:59 -0400

    Centering footer and padding

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 import "./Footer.style.scss";
 
 function Footer() {
-  return <div className="footer">© 2023 Carleton University</div>;
+  return <div className="footer">
+    © 2023 Carleton University
+    </div>;
 }
 
 export default Footer;
