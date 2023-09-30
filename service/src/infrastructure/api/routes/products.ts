9a99410b5550f69e5c0b68f61d98df02f0ddafc5
Jace222 <jasonscs222@gmail.com>
2023-09-30 15:00:33 -0400

    Did Delete Product Endpoint
    HAHAHAHAH I WAS TROLLING THE WHOLE TIME

--- service/src/infrastructure/api/routes/products.ts
+++ service/src/infrastructure/api/routes/products.ts
@@ -6,7 +6,7 @@ import { success, error, verifyAuthorization } from "../utils";
 const router = express.Router();
 
 const getProducts = async (_: Request, response: Response) => {
-  const products = await ProductService.all();
+  const products = await ProductService.all(); // Filter out deleted products
 
   return success(response, {
     data: {
@@ -24,6 +24,11 @@ const getProduct = async (request: Request, response: Response) => {
     return error(response, {
       error: "Product not found.",
       statusCode: 404,
+    });
+  } else if (product.deleted) {
+    return error(response, {
+      error: "Product is deleted.",
+      statusCode: 404, // You can use a different status code if desired
     });
   }
 
@@ -101,9 +106,51 @@ const updateProduct = async (request: Request, response: Response) => {
   }
 };
 
+const softDeleteProduct = async (request: Request, response: Response) => {
+  // Extract product ID from the URL parameters
+  const id = request.params.id;
+
+  const authorization = await verifyAuthorization(
+    request.headers.authorization,
+  );
+
+  if (authorization.err) {
+    return error(response, {
+      error: authorization.val.message,
+      statusCode: 401,
+    });
+  }
+
+  try {
+    // Use ProductService to soft delete the product by setting the 'deleted' flag to true
+    const updatedProduct = await ProductService.softDeleteProduct(id);
+
+    if (!updatedProduct) {
+      return error(response, {
+        error: "Product not found.",
+        statusCode: 404,
+      });
+    }
+
+    return success(response, {
+      data: {
+        product: updatedProduct,
+      },
+      statusCode: 200,
+    });
+  } catch (error) {
+    return error(response, {
+      error: "Error soft deleting product.",
+      statusCode: 500,
+    });
+  }
+};
+
+
 router.get("/", getProducts);
 router.get("/:id", getProduct);
 router.post("/", createProduct);
 router.put("/:id", updateProduct); // Add the new PUT endpoint for updating a product
+router.delete("/:id", softDeleteProduct);
 
 export default router;
